e0719a27aac232eabb6632b10a9116bdd05eb9cd
Isabella18081996 <Isabella.Nita@yahoo.it>
2021-05-19 22:04:00 +0200

    esercizio con bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="jumbotron">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+
+}
+</script>
+
+<style>
+.jumbotron{
+  width:80%;
+  margin: auto;
+  height: 200px;
+  background-color: aquamarine;
+  border-radius: 10px;
+}
+</style>
